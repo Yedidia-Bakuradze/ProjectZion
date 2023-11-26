526f2bf126c8fcfe00d21a4ba5839fbda2ca616e
Yedidia Bakuradze <yedidiadev@gmail.com>
2023-11-26 16:21:17 +0200

    Normal speed and extra speed added.

--- Assets/MapAssets/Scripts/Player.cs
+++ Assets/MapAssets/Scripts/Player.cs
@@ -4,8 +4,12 @@ namespace MapAssets.Scripts
 {
     public class Player : MonoBehaviour
     {
+        [SerializeField] private float movingSpeed = 5f;
+        [SerializeField] private float extraSpeed = 1.75f;
+        [SerializeField] private bool IsFastMode;
         private void Update()
         {
+            IsFastMode = false;
             Vector2 inputDirection = new Vector2();
             if (Input.GetKey(KeyCode.W))
             {
@@ -24,10 +28,21 @@ namespace MapAssets.Scripts
                 inputDirection.x++;
             }
 
-            inputDirection = inputDirection.normalized;
-            Vector3 moveDirection = new Vector3(inputDirection.x,0f,inputDirection.y);
-            transform.position += moveDirection*Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                IsFastMode = true;
+            }
 
+            inputDirection = inputDirection.normalized;
+            transform.position += GetDirectionVector(inputDirection);
+        }
+
+        private Vector3 GetDirectionVector(Vector2 inputVector)
+        {
+            Vector3 moveDirectionVector = new Vector3(inputVector.x,0f,inputVector.y)*(Time.deltaTime*movingSpeed);
+            if (IsFastMode)
+                moveDirectionVector *= extraSpeed;
+            return moveDirectionVector;
         }
     }
 }
